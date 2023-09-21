14341186f70d8e795c668236674b5a6662cb0235
zain shaikh <139116312+zainshaikh017@users.noreply.github.com>
2023-09-22 01:04:30 +0500

    first commit

---
+++ lib/Auth/SignUp.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+      body: Column(
+        children: [
+
+          Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter name'
+                    ),
+                  )
+
+
+          ],))
+
+
+        ],
+      ),
+
+
+    );
+  }
+}
